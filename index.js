8c072f9112839cadec5e5e8cb11254ae862abc2d
mezgoodle <mezgoodle@gmail.com>
2020-04-11 09:38:50 +0300

    Response on greetings

--- index.js
+++ index.js
@@ -4,26 +4,30 @@ const TelegramBot = require("node-telegram-bot-api");
 const token = "YOUR_TELEGRAM_BOT_TOKEN";
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
 });
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+    const chatId = msg.chat.id;
+    if (msg.text.toString().toLowerCase().includes("hi") || msg.text.toString().toLowerCase().includes("hello")) {
+        let str = `Hello, ${msg.from.first_name}. I\'m bot for showing weather information by using [OpenWeatherMap](https://openweathermap.org/) API.\nMy creator is @sylvenis. Also my code is [here](https://github.com/mezgoodle/weather-bot).\nGood luck!😉`
+        bot.sendMessage(chatId, str, { parse_mode: "Markdown" });
+    }
+    if (msg.text.toString().toLowerCase().includes("bye")) {
+        bot.sendMessage(chatId, "Have a nice day, " + msg.from.first_name);
+    }
 });
