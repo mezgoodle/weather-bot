ea33270050689b670f3fdc78c2678d18c97b00ab
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-04-25 18:16:01 +0300

    Add testing

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,9 +2,9 @@ const data = require("./data.json");
 const { fetchData } = require("./util");
 
 describe.each(data.current_weather)("Get weather info:", (city, expected) => {
-    test("Checking fetch data", () => {
+    test("testing by country", () => {
         fetchData(city).then(data => {
-            expect(data).toEqual(expected);
-        });
+            expect(data.sys.country).toEqual(expected);
+        })
     });
 });
--- test/util.js
+++ test/util.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
-
+require("dotenv").config();
+const api_key = process.env.API_KEY;
 const fetchData = (city) => {
     return axios
-        .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&appid=${process.env.API_KEY}`)
+        .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&appid=${api_key}`)
         .then(response => {
             return response.data;
         });
