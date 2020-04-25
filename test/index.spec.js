885d0d11c9f525ee04528ab136bdd95d118a5795
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-04-25 17:14:13 +0300

    Add promise to fetch function

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,10 @@
-const axios = require("axios");
 const data = require("./data.json");
 const { fetchData } = require("./util");
 
 describe.each(data.current_weather)("Get weather info:", (city, expected) => {
     test("Checking fetch data", () => {
-        expect(fetchData(city)).toBe(expected);
+        fetchData(city).then(data => {
+            expect(data).toEqual(expected);
+        });
     });
 });
