befc3d235ddae4a3eeb3bc5a161702c3a7700479
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-13 20:41:49 +0300

    Add testing by coords

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,22 @@
 const data = require("./data.json");
-const { fetchData, convertTime } = require("./util");
+const { fetchDataCity, fetchDataCoords, convertTime } = require("./util");
 
-describe.each(data.current_weather)("Getting weather info:", (city, expected) => {
+describe.each(data.current_weather)("Getting weather info by city name:", (city, expected, coords) => {
     test("testing by country (now)", () => {
-        fetchData(city, "weather").then(data => {
+        fetchDataCity(city, "weather").then(data => {
             expect(data.sys.country).toEqual(expected);
         })
     });
 
     test("testing by country (tommorow)", () => {
-        fetchData(city, "forecast").then(data => {
+        fetchDataCity(city, "forecast").then(data => {
             expect(data.city.country).toEqual(expected);
+        })
+    });
+
+    test("testing by coords", () => {
+        fetchDataCoords(coords.lat, coords.lon).then(data => {
+            expect(data.sys.country).toEqual(expected);
         })
     });
 });
--- test/util.js
+++ test/util.js
@@ -3,9 +3,18 @@
 const axios = require("axios");
 require("dotenv").config();
 const api_key = process.env.API_KEY;
-const fetchData = (city, variant) => {
+const fetchDataCity = (city, variant) => {
     return axios
         .get(`http://api.openweathermap.org/data/2.5/${variant}?q=${city}&units=metric&&appid=${api_key}`)
+        .then(response => {
+            return response.data;
+        })
+        .catch(err => console.log(err));
+};
+
+const fetchDataCoords = (lat, lon) => {
+    return axios
+        .get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&&appid=${api_key}`)
         .then(response => {
             return response.data;
         })
@@ -22,4 +31,4 @@ const convertTime = (timestamp) => {
     return formattedTime;
 }
 
-module.exports = { fetchData, convertTime };
+module.exports = { fetchDataCity, fetchDataCoords, convertTime };
