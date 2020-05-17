59d73131d327ae4dec2fdf1deba7902d74910ac0
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-17 12:37:38 +0300

    Set chat_id as secret

--- test/util.js
+++ test/util.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 require("dotenv").config();
 const api_key = process.env.API_KEY;
 const token = process.env.TELEGRAM_TOKEN;
+const chat_id = process.env.CHAT_ID;
 const fetchDataCity = (city, variant) => {
     return axios
         .get(`http://api.openweathermap.org/data/2.5/${variant}?q=${city}&units=metric&&appid=${api_key}`)
@@ -24,7 +25,7 @@ const fetchDataCoords = (lat, lon) => {
 
 const fetchAPITelegram = (method, data) => {
     return axios
-        .get(`https://api.telegram.org/bot${token}/${method}?chat_id=353057906&${data}`)
+        .get(`https://api.telegram.org/bot${token}/${method}?chat_id=${chat_id}&${data}`)
         .then(response => {
             return response.data;
         })
