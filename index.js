d6109fa26731f5ab5fa3d1a3903bd060cf372a65
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-25 12:40:44 +0300

    Improve convertTime function

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const urls = {
     tomorrow: "forecast"
 };
 
-// Days and months
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// Months
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 // Connect to Mongo
@@ -114,12 +113,14 @@ const getWeather = (chatId, city, choice, coords) => {
 // Convert time from timstamp to string
 const convertTime = (timestamp) => {
     const date = new Date(timestamp * 1000);
-    const day = days[date.getDay()];
     const month = months[date.getMonth()];
+    const day = String(date.getDate()).padStart(2, '0');
+    const year = date.getFullYear();
+    const output = month + ' ' + day + ',' + year;
     const hours = date.getHours();
     const minutes = "0" + date.getMinutes();
     const seconds = "0" + date.getSeconds();
-    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + " " + day + " " + month;
+    const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2) + " " + output;
     return formattedTime;
 }
 
@@ -268,3 +269,4 @@ bot.on("text", (msg) => {
 
 // Listen for errors
 bot.on("polling_error", (err) => console.log(err));
+module.exports = { convertTime, months };
