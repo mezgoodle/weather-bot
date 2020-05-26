4bac407200a52dc83e4d8fcb0c85de394e9a85c1
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-05-26 18:45:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ const token = process.env.TELEGRAM_TOKEN;
 const api_key = process.env.API_KEY;
 // MongoDB database config
 const dbURI = `mongodb+srv://mezgoodle:${process.env.DB_PASS}@weather-user-data-suiox.mongodb.net/test?retryWrites=true&w=majority`;
+// Geocode api key
+const geo_api_key = process.env.GEOCODE;
 
-module.exports = { token, api_key, dbURI };
+module.exports = { token, api_key, dbURI, geo_api_key };
