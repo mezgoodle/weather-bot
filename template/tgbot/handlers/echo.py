f74b1b074cf658028614f31836f9f84ea38c7cc1
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-18 09:36:27 +0300

    Update echo.py

--- template/tgbot/handlers/echo.py
+++ template/tgbot/handlers/echo.py
@@ -1,12 +1,19 @@
 from aiogram.types import Message
+from aiogram.dispatcher.filters.builtin import Text
 
 from loader import dp
 
 import logging
 
 
-@dp.message_handler()
+@dp.message_handler(Text(equals=['hi', 'hello'], ignore_case=True))
 async def echo(message: Message) -> Message:
     logger = logging.getLogger(__name__)
     logger.info('Handler executed')
-    await message.answer(message.text)
+    sender_name = message.from_user.first_name
+    text = f'Hello, {sender_name}. I\'m bot for showing weather information by using <a ' \
+           f'href="https://openweathermap.org/">OpenWeatherMap' \
+           f'</a> API.\nMy creator is @sylvenis. ' \
+           f'Also my code is <a href="https://github.com/mezgoodle/weather-bot">here' \
+           f'</a>.\nGood luck!😉'
+    return await message.answer(text)
