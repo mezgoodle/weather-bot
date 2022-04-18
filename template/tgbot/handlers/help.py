ec451774dd12eb3e2279368b13cc9bd4a6d1ff22
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-18 09:40:27 +0300

    Update help.py

--- template/tgbot/handlers/help.py
+++ template/tgbot/handlers/help.py
@@ -2,7 +2,6 @@ from aiogram.types import Message
 from aiogram.dispatcher.filters import CommandHelp
 
 from loader import dp
-from tgbot.keyboards.inline.help_keyboard import create_markup
 
 import logging
 
@@ -11,6 +10,17 @@ import logging
 async def help_command(message: Message) -> Message:
     logger = logging.getLogger(__name__)
     logger.info('Handler executed')
-
-    markup = create_markup()
-    await message.reply(f'Hello, {message.from_user.username}!', reply_markup=markup)
+    text = '''
+    Hi!
+Here you can see commands that you can type for this bot:
+/now <b>city</b> - get weather information in city.
+/tomorrow <b>city</b> - get weather information in city for tomorrow.
+/week <b>city</b> - get weather information in city for 7 days.
+/lang <b>lang_code</b> - set language information in database for
+getting main weather info in native language
+/set <b>city</b> - sets in database selected city.
+/w - get weather information in city by language that you set in database for 3 days.
+/location - get actual information in the city by geographical point.
+/help - look for available commands
+    '''
+    return await message.reply(text)
