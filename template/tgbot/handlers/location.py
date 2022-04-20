85db316a12f0fb6c4da6195a34dee0f8f8cb74e5
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-20 09:17:06 +0300

    Update location.py

--- template/tgbot/handlers/location.py
+++ template/tgbot/handlers/location.py
@@ -1,4 +1,4 @@
-from aiogram.types import Message
+from aiogram.types import Message, ContentTypes
 
 from loader import dp
 from tgbot.keyboards.reply import location_keyboard
@@ -12,3 +12,11 @@ async def location_command(message: Message) -> Message:
     logger.info('Handler executed')
     markup = location_keyboard.create_markup()
     return await message.reply('Send me location by button', reply_markup=markup)
+
+
+@dp.message_handler(content_types=ContentTypes.LOCATION)
+async def location_handler(message: Message) -> Message:
+    logger = logging.getLogger(__name__)
+    logger.info('Handler executed')
+    print(message.location)
+    return await message.reply(f'You sent location: {message.location}')
