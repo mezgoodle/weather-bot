20540a79edb4f294c6d1d1096c928896cc26dd75
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-23 15:42:49 +0300

    Update database.py
    Add logging

--- template/tgbot/misc/database.py
+++ template/tgbot/misc/database.py
@@ -2,31 +2,39 @@ import motor.motor_asyncio
 
 from tgbot.config import load_config
 
+import logging
+
 config = load_config()
+logger = logging.getLogger(__name__)
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
-    f"mongodb+srv://mezgoodle:{config.db.password}@weather-user-data-suiox.mongodb.net/test?retryWrites=true&w=majority"
+    f'mongodb+srv://mezgoodle:{config.db.password}@weather-user-data-suiox.mongodb.net/test?retryWrites=true&w=majority'
 )
 db = client[config.db.database]
 
 
 async def create_document(data: dict, collection: str) -> bool:
     new_object = await db[collection].insert_one(data)
-    created_object = await db[collection].find_one({"_id": new_object.inserted_id})
+    created_object = await db[collection].find_one({'_id': new_object.inserted_id})
     if not created_object:
+        logger.info(f'Failed to create object: {data}')
         return False
+    logger.info(f'Created new object: {created_object}')
     return True
 
 
 async def get_all_objects(collection: str, query: dict = None) -> list:
     objects = await db[collection].find(query).to_list(1000)
     if not objects:
+        logger.info(f'Failed to get objects: {query}')
         return []
+    logger.info(f'Found {len(objects)} objects')
     return objects
 
 
 async def get_object(query: dict, collection: str) -> dict:
     if (founded_object := await db[collection].find_one(query)) is not None:
+        logger.info(f'Found object: {founded_object}')
         return founded_object
     return {}
 
@@ -34,7 +42,7 @@ async def get_object(query: dict, collection: str) -> dict:
 async def update_object(query: dict, requested_object: dict, collection: str) -> dict:
     if len(requested_object) >= 1:
         update_result = await db[collection].update_one(
-            query, {"$set": requested_object}
+            query, {'$set': requested_object}
         )
 
         if update_result.modified_count == 1:
@@ -43,12 +51,13 @@ async def update_object(query: dict, requested_object: dict, collection: str) ->
                 value = requested_object[key]
                 query = {key: value}
             if (updated_object := await db[collection].find_one(query)) is not None:
+                logger.info(f'Updated object: {updated_object}')
                 return updated_object
         return {}
 
     if (existing_object := await db[collection].find_one(query)) is not None:
         return existing_object
-
+    logger.info(f'Failed to update object: {query}')
     return {}
 
 
@@ -56,5 +65,7 @@ async def delete_object(query: dict, collection: str) -> bool:
     delete_result = await db[collection].delete_one(query)
 
     if delete_result.deleted_count == 1:
+        logger.info(f'Deleted object: {query}')
         return True
+    logger.info(f'Failed to delete object: {query}')
     return False
