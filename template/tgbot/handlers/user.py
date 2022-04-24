9484384eb19b9e7b6b48f7fc6e699b8a63464698
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-24 21:03:52 +0300

    Update user.py
    Create handler for set command

--- template/tgbot/handlers/user.py
+++ template/tgbot/handlers/user.py
@@ -21,3 +21,19 @@ async def set_lang_handler(message: Message) -> Message:
         return await message.answer(f'{message.from_user.first_name}, your language has been set to {lang}')
     else:
         return await message.answer('Something went wrong')
+
+
+@dp.message_handler(RegexpCommandsFilter(['\/set (.+)']))
+async def set_lang_handler(message: Message) -> Message:
+    logger = logging.getLogger(__name__)
+    logger.info('Handler executed')
+    try:
+        city = message.text.split(' ')[1]
+    except IndexError:
+        await message.answer('Please provide city name')
+    user_id = message.from_user.id
+    result = await update_object({'user_id': user_id}, {'city': city}, user_id)
+    if result:
+        return await message.answer(f'{message.from_user.first_name}, your information has been saved')
+    else:
+        return await message.answer('Something went wrong')
